2400ca63ab3c8dfe4d0649c43cf3e95b1326d7c8
Adriano Dias <adriano.rj.br@gmail.com>
2023-05-17 14:23:15 -0300

    implements tests for listBooks() on BookService.

--- src/test/java/com/adriano/testsfinalproject/services/BookServiceTests.java
+++ src/test/java/com/adriano/testsfinalproject/services/BookServiceTests.java
@@ -2,6 +2,7 @@ package com.adriano.testsfinalproject.services;
 
 import com.adriano.testsfinalproject.helpers.BookFactory;
 import com.adriano.testsfinalproject.models.Book;
+import com.adriano.testsfinalproject.models.BookDto;
 import com.adriano.testsfinalproject.repositories.BookRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -9,10 +10,10 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -26,7 +27,7 @@ class BookServiceTests {
 
     @Test
     void mustRemoveABookThatExists() {
-Book bookTest = BookFactory.fakeBook("Meu Livro");
+        Book bookTest = BookFactory.fakeBook("Meu Livro");
         var existingId = bookTest.getId();
         when(bookRepository.findById(existingId)).thenReturn(Optional.of(bookTest));
         doNothing().when(bookRepository).delete(any(Book.class));
@@ -63,4 +64,29 @@ Book bookTest = BookFactory.fakeBook("Meu Livro");
         assertThrows(IllegalArgumentException.class, () -> bookService.findById(wrongId));
     }
 
+    @Test
+    void mustListAllBooks() {
+        List<Book> books = List.of(
+                BookFactory.fakeBook("Livro 1"),
+                BookFactory.fakeBook("Livro 2"),
+                BookFactory.fakeBook("Livro 3")
+        );
+
+        when(bookRepository.findAll()).thenReturn(books);
+
+        var answer = bookService.listBooks();
+
+        assertEquals(3, answer.size());
+        assertEquals(books.get(1).getTitle(), answer.get(1).title());
+    }
+
+    @Test
+    void mustListAllBooksReturnsEmptyWhenNoBook() {
+        List<Book> books = List.of();
+        when(bookRepository.findAll()).thenReturn(books);
+
+        var answer = bookService.listBooks();
+
+        assertTrue(answer.isEmpty());
+    }
 }
